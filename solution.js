972b16718a538baf3415c53583a1effb5be5fd06
Cem <cemkgrbz@gmail.com>
2022-06-29 14:03:16 +0200

    modified js file

--- solution.js
+++ solution.js
@@ -1,1 +1,36 @@
+//1
+
+const str = "123456"
+console.log(str)
+
+//2
+
+console.log('----------')
+console.log('2nd element is', str[1])
+console.log('6th element is', str[5])
+
+//3 - 4 - 5 - 6
+
+console.log('----------')
+const firstName = "Cem"
+const age = "32"
+const year = "2022"
+console.log(firstName + " is " + age + " in " + year)
+
+//7 - 8
+
+console.log('----------')
+const isMarried = [false]
+console.log("Cem is married: " + isMarried)
+
+//9 - 10 - 11
+
+console.log('----------')
+const programmingLanguage = "JavaScript"
+const isFun = [true]
+console.log(programmingLanguage + " is fun: " + isFun)
+console.log(programmingLanguage[0],programmingLanguage[4])
+console.log(programmingLanguage.length)
+
+
 
